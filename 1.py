45dd3bf4b190fe049fdb1ff4bfd8ee8634dd4dfd
polinabog475@gmail.com <polinabog475@gmail.com>
2022-09-30 16:00:01 +0300

    Initial commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+name=input("Имя студента")
+sername=input("Фамилия студена")
+C=int(input("Год рождения"))
+A, B= B, A
+C+=60
+#print(A,B,C, sep="_")
+print(A,B,C, sep="_")
---
+++ 2.py
@@ -0,0 +1,6 @@
+katet1=int(input())
+katet2=int(input())
+gipotenuza=int(input())
+pirimetr=katet1+katet2+gipotenuza
+ploshad=0.5*(katet1*katet2)
+print(ploshad,pirimetr)
