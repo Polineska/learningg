2fce5d9cb4cc5662342dbf4825d37cdfc265d904
polinabog475@gmail.com <polinabog475@gmail.com>
2022-10-06 21:09:02 +0300

    Create 3.py

---
+++ ср3/3.py
@@ -0,0 +1,10 @@
+#radius=int(input("радиус круга ="))
+#pi=3.14
+#plosh=pi*((radius)**2)
+#print(plosh)
+#с вызовом библеотеки
+import math
+pi=math.pi
+radius=int(input("радиус круга ="))
+plosh=pi*((radius)**2)
+print(" площадь равна = ", plosh)
